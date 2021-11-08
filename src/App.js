e0a12f139117cbd7d15060eac8d9ab72120dda7d
Harrison <h.alexander.barnett@gmail.com>
2021-11-08 15:25:34 -0500

    browserrouter import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 import './App.css'
 
 const App = ()=> {
   return (
     <>
+      <Header />
+      <div id='page-content'>
+        
+      </div>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Navigation from './Navigation'
+const Header = () => {
+    return(
+        <>
+            <h1>Header</h1>
+            <Navigation />
+        </>
+    )
+}
+export default Header
